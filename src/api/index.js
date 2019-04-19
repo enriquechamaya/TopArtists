f0a9c21bd237fd251e8eca8fb46ba6922e459dc8
Enrique Chamaya <enrique.chamaya.delacruz@gmail.com>
2019-04-18 23:41:05 -0500

    fixes api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import config from './config';
 const API_KEY = config.apiKey;
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${API_KEY}&format=json`;
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${API_KEY}&format=json`;
 
 
 export default function getArtists(country) {
@@ -8,5 +8,5 @@ export default function getArtists(country) {
     return fetch(url)
         .then(res => res.json())
         .then(json => json.topartists.artist)
-        .catch(error => alert('Ocurrió un error con el país seleccionado, por favor eliga otro. ' + error))
+        .catch(error => console.log('Ocurrió un error con el país seleccionado, por favor eliga otro. ' + error))
 }
